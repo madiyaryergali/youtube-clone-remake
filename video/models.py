0d9d99eb78b21e6b83e2204b4d825c3e67804341
Madiyar <evs.mako@gmail.com>
2023-07-12 11:09:04 +0600

    Initialized Video model

--- video/models.py
+++ video/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from channel.models import Channel
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+class Video(models.Model):
+    video = models.FileField(upload_to='static/videos_uploaded', validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=50, null=True, blank=True)
+    channel_id = models.OneToOneField(Channel, on_delete=models.CASCADE)
+
+
+    def __str__(self) -> str:
+        return str(self.pk) + ' : ' + self.name
