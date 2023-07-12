df21e2d18c872fa1c1eb42f499621fdb0720508a
Madiyar <evs.mako@gmail.com>
2023-07-12 11:08:49 +0600

    Initialized SavedVideos model

--- savedVideos/models.py
+++ savedVideos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from video.models import Video
 
-# Create your models here.
+class SavedVideos(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    video = models.ManyToManyField(Video)
