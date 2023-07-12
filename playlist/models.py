2345bbcd9198c3a7b870e8fa2e8caa1137035551
Madiyar <evs.mako@gmail.com>
2023-07-12 11:08:39 +0600

    Initialized Playlist model

--- playlist/models.py
+++ playlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from video.models import Video
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Playlist(models.Model):
+    name = models.CharField(max_length=50)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.ManyToManyField(Video)
+
+    def __str__(self) -> str:
+        return str(self.pk) + ' : ' + self.name
