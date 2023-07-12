eb891397fb716a7914b22df0d982cb2825452950
Madiyar <evs.mako@gmail.com>
2023-07-12 11:09:13 +0600

    Initialized WatchHistory model

--- watchHistory/models.py
+++ watchHistory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from video.models import Video
 
-# Create your models here.
+class WatchHistory(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    watch_date = models.DateTimeField(auto_now_add=True)
+    video_id = models.ManyToManyField(Video)
+
