17495f46324164c9145063609a50fda7d4da9e15
Madiyar <evs.mako@gmail.com>
2023-07-12 11:08:58 +0600

    Initialized SearchHistory model

--- searchHistory/models.py
+++ searchHistory/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SearhHistory(models.Model):
+    user_id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    prompt = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
