93a36d38263998f70e872904c64dc0aa3fd7c07e
Madiyar <evs.mako@gmail.com>
2023-07-12 11:08:29 +0600

    Initialized Comment model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from video.models import Video
 
-# Create your models here.
+class Comment(models.Model):
+    comment_text= models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.ForeignKey(Video, on_delete=models.CASCADE)
+
