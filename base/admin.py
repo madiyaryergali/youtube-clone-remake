b139135ef1ed6c3fb3b6edf626cc578cf89bce04
Madiyar <evs.mako@gmail.com>
2023-07-12 11:22:48 +0600

    Registered all models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from channel.models import Channel
+from comment.models import Comment
+from playlist.models import Playlist
+from savedVideos.models import SavedVideos
+from searchHistory.models import SearhHistory
+from video.models import Video
+from watchHistory.models import WatchHistory
 
-# Register your models here.
+admin.site.register(Channel)
+admin.site.register(Comment)
+admin.site.register(Playlist)
+admin.site.register(SavedVideos)
+admin.site.register(SearhHistory)
+admin.site.register(Video)
+admin.site.register(WatchHistory)
