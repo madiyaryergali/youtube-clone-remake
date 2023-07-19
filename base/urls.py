9373ab1de334e5cd97e01f06cc7de558b9e8e546
Madiyar <evs.mako@gmail.com>
2023-07-19 16:33:51 +0600

    Changed login and logout paths

--- base/urls.py
+++ base/urls.py
@@ -4,10 +4,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 from .forms import LoginForm
 
+app_name = 'base'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('home/', views.index, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='base/login.html', authentication_form=LoginForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.log_out, name='logout'),
 ]
