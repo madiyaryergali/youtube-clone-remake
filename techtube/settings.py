73c19cddf6adc41173db0ebbb96fd722e762994e
Madiyar <evs.mako@gmail.com>
2023-07-19 21:26:02 +0600

    Debug False

--- techtube/settings.py
+++ techtube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5o77y_qa=0w3z!-86#mr)=a26w@71%z^0&a_!7y^7e6s4)eb%6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['techtube.azurewebsites.net', 'https://techtube.azurewebsites.net', 'localhost']
 
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
