b2c2b207623a881be4ac0666b8e454ba651cc0cc
Madiyar <evs.mako@gmail.com>
2023-07-19 16:40:16 +0600

    Settings.py adjustments and models

--- channel/models.py
+++ channel/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Channel(models.Model):
     name = models.CharField(max_length=50)
-    logo = models.ImageField(upload_to='static/images_uploaded', null=True, blank=True)
-    background = models.ImageField(upload_to='static/images_uploaded', null=True, blank=True)
+    logo = models.ImageField(upload_to='images_uploaded', null=True, blank=True)
+    background = models.ImageField(upload_to='images_uploaded', null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
--- video/models.py
+++ video/models.py
@@ -3,7 +3,7 @@ from channel.models import Channel
 from django.core.validators import FileExtensionValidator
 
 class Video(models.Model):
-    video = models.FileField(upload_to='static/videos_uploaded', validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    video = models.FileField(upload_to='videos_uploaded/%y', validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
     name = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     upload_date = models.DateTimeField(auto_now_add=True)
