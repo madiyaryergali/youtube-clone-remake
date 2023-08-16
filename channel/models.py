7e52517c212a1dca3afdcfa12d6e332c85c6db32
Madiyar <evs.mako@gmail.com>
2023-08-16 15:17:59 +0600

    Creating new channel with new user

--- channel/models.py
+++ channel/models.py
@@ -1,3 +1,5 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,7 +9,16 @@ class Channel(models.Model):
     background = models.ImageField(upload_to='images_uploaded', null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name='channel')
 
     def __str__(self):
         return str(self.pk) + ' : ' + self.name
+    
+@receiver(post_save, sender=User)
+def create_user_channel(sender, instance, created, **kwargs):
+    
+    if created:
+        Channel.objects.create(user_id=instance, name=f"{instance.username}'s Channel")
+
+
+post_save.connect(create_user_channel, sender=User)
