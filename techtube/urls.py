6074f2fd1cd6a3a2e2dc5b38e683d1853c38c569
Madiyar <evs.mako@gmail.com>
2023-08-16 15:19:59 +0600

    added channel.urls.py

--- techtube/urls.py
+++ techtube/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('videos/', include('video.urls')),
     path('', include('base.urls')),
+    path('channel/', include('channel.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
