6debc0920747caf5962c88b719a2d79b7289ee7b
Madiyar <evs.mako@gmail.com>
2023-08-16 15:22:52 +0600

    added search, comment, detail views

--- video/views.py
+++ video/views.py
@@ -2,19 +2,42 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Video
 from django.contrib.auth.decorators import login_required
 from .forms import NewVideoForm
+from comment.models import Comment
+
+from django.db.models import Q
+
+def search(request):
+    search_query = request.GET.get('search_query')
+
+    if search_query:
+        videos = Video.objects.filter(Q(name__icontains=search_query) | Q(channel_id__name__icontains=search_query))
+    else:
+        videos = Video.objects.all()
+
+    context = {'videos': videos}
+    return render(request, 'video/search.html', context)
+
 
 def videos(request):
     videos = Video.objects.all()
     return render(request, 'video/videos.html', {'videos': videos,})
 
-def detail(request, pk):
-    item = get_object_or_404(Video, pk=pk)
-    #related_items = Video.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+def video_detail(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+    comments = Comment.objects.filter(video_id=pk)
+    return render(request, 'video/video_detail.html', {'video': video, 'comments':comments})
 
-    return render(request, 'item/detail.html', {
-        'item': item,
-        #'related_items': related_items,
-    })
+@login_required
+def add_comment(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+
+    if request.method == 'POST':
+        comment_text = request.POST.get('comment_text')
+        if comment_text:
+            Comment.objects.create(video_id=video, comment_text=comment_text, user_id=request.user)
+
+    return redirect('video:video_detail', pk=pk)
+
 
 @login_required
 def new(request):
